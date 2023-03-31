f3ba5fab496349a7cbf8a1cfda3fc14c654ff76e
MattAGUT <mapache.tn@gmail.com>
2023-03-30 22:09:12 -0400

    Added one random function

--- fakemap/fakemap.py
+++ fakemap/fakemap.py
@@ -1,1 +1,12 @@
 """Main module."""
+
+import random
+
+# Generate random latitude and longitude coordinates
+def Generate_random_location():
+    lat = random.uniform(-90, 90)
+    lon = random.uniform(-180, 180)
+
+    # Print the coordinates
+    print("Latitude: ", lat)
+    print("Longitude: ", lon)
